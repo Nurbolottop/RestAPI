ce63c6d1e33cb9c2c9cfe86e58ce3734845f8913
Nurbolottop <erk1nbaevw2711@gmail.com>
2023-02-26 06:30:49 -0500

    Readme.md

--- apps/cards/urls.py
+++ apps/cards/urls.py
@@ -9,7 +9,7 @@ router.register(prefix='comment', viewset=CommentAPIViewSet)
 urlpatterns = [
     path('api/card/', CardAPI.as_view(), name="api_Card"),
     path('api/card/<int:pk>', CardDetailAPI.as_view(), name="api_card"),
-    path('api/search/', CardSearchAPIView.as_view(), name='search'),
+    path('api/card/search/', CardSearchAPIView.as_view(), name='search'),
     
 ]
 urlpatterns += router.urls
